20b24b91b6792a6f576648d443335fd89508e8eb
cotaluis <135561226+cotaluis@users.noreply.github.com>
2023-10-25 14:42:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
+const fs = require('fs');
+const SVG = require('svg.js');
+const inquirer = require('inquirer');
 
+async function getUserInput() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'output',
+            message: 'Enter the output SVG file path:',
+            default: 'output.svg',
+        },
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter the text for the logo:',
+            default: 'Hello, Logo!',
+        },
+    ]);
+}
+
+function generateLogo(text) {
+    const width = 200;
+    const height = 100;
+
+    const canvas = createCanvas(width, height);
+    const svg = SVG(canvas);
+
+    svg.rect(width, height).fill('#3498db');
+    svg.text(text).move(width / 4, height / 2).font({ size: 20, family: 'Arial', fill: '#ffffff' });
+
+    return svg;
+}
+
+function saveLogoToFile(svg, outputPath) {
+    const svgString = svg.svg();
+    fs.writeFileSync(outputPath, svgString, 'utf-8');
+    console.log(`Logo saved to ${outputPath}`);
+}
+
+async function main() {
+    const userInput = await getUserInput();
+    const { output, text } = userInput;
+
+    const logo = generateLogo(text);
+    saveLogoToFile(logo, output);
+}
+
+main();
